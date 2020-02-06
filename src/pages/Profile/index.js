1c66f45b88e75f52584b843e943a1dbecb7e2329
Jeferson GB <jefersongb@outlook.com.br>
2020-02-05 23:27:17 -0300

    Cria o componente e a estilização Appointment.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,9 +3,16 @@ import { View } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
+import { Container, Title } from './styles';
 
 export default function Profile() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+      </Container>
+    </Background>
+  );
 }
 
 Profile.navigationOptions = {
