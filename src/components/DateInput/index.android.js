bef893fe2c0426c733edac5a1f79e0c4912d3c36
Jeferson GB <jefersongb@outlook.com.br>
2020-02-08 01:17:26 -0300

    Cria a confirmação do agendamento e configura este agendamento na api.

--- src/components/DateInput/index.android.js
+++ src/components/DateInput/index.android.js
@@ -2,12 +2,13 @@ import React, { useMemo } from 'react';
 import { DatePickerAndroid } from 'react-native';
 import { format } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, DateButton, DateText } from './styles';
 
 export default function DateInput({ date, onChange }) {
   const dateFormatted = useMemo(
-    () => format(date, "dd 'de MMMM 'de' yyyy", { locale: pt }),
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
     [date]
   );
 
